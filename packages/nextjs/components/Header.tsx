d1ce743d6b03efb86b60cec61b2dd6dd4ea02532
Santiago Gonzalez <hernangt12re3@gmail.com>
2025-10-29 15:46:19 -0500

    update thumbnail + header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -89,8 +89,8 @@ export const Header = ({ isTransparent = false }: { isTransparent?: boolean }) =
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Speedrun Ethereum</span>
+            <span className="text-xs">Built with Batch 21 Builders</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
